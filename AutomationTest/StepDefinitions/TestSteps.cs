f7fdcd708a53cb060f095b2e4134dc71c6978daf
Kierownik <filipmodelski@gmail.com>
2023-11-18 23:36:00 +0100

    adfs

--- AutomationTest/StepDefinitions/TestSteps.cs
+++ AutomationTest/StepDefinitions/TestSteps.cs
@@ -9,7 +9,7 @@ namespace AutomationTest.StepDefinitions
         [Given(@"Go to sport\.pl page")]
         public void GivenGoToSport_PlPage()
         {
-            AutomationTest.Hooks.Hook.Driver!.Navigate().GoToUrl("https://stackoverflow.com/questions/12211781/how-to-maximize-window-in-chrome-using-webdriver-python");
+            //AutomationTest.Hooks.Hook.Driver!.Navigate().GoToUrl("https://stackoverflow.com/questions/12211781/how-to-maximize-window-in-chrome-using-webdriver-python");
         }
 
     }
